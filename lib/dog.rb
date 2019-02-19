b5b4cf1016aac32ab041687c86303b9f66ba1394
Alicia Santiago <albrown07@mac.com>
2019-02-19 15:21:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
     breed_default
     end
     
-  def breed_default
-    puts "Mutt"
-  end
+  #def breed_default
+   # puts "Mutt"
+  #end
 
 end
